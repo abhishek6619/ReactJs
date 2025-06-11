fde5fddda6bc4cc71c4d30c2cfde3f29c6ee8264
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-06-11 11:06:31 +0530

    Update

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
 import React from 'react'
@@ -5,8 +6,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Chai</h1>
+    </div>
+  ) 
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <StrictMode>
-    <App />
   // </StrictMode>
+    MyApp()
 )
