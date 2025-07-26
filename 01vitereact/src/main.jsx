cf45e2c4a51b5c7127ca0095cfdbd344a5ba3205
Abhishek Kumar Singh <abhis42124@gmail.com>
2025-07-26 09:20:24 +0530

    Add code

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,21 +1,40 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-refresh/only-export-components */
 // import { StrictMode } from 'react'
 // import { createRoot } from 'react-dom/client'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 // import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
 function MyApp() {
   return (
     <div>
       <h1>Custom App | Chai</h1>
+      <h2>Abhishek Singh</h2>
     </div>
-  ) 
+  );
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  // </StrictMode>
-    MyApp()
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://www.google.com",
+//     target: "_blank",
+//     children: "Click me to visit Google",
+//   },
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const reactElement = React.createElement(
+  
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  anotherElement()
+);
